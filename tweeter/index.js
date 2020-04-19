87e155b346b2ebf90f7f6b0e714cf3c30b8e0455
Anki <anki@Ankis-MacBook-Pro.local>
2020-04-19 13:34:27 -0700

    setup twatter

---
+++ tweeter/index.js
@@ -0,0 +1,17 @@
+const Twit = require('twit');
+const privateConf = require('./private-conf');
+
+const twat = new Twit({
+    consumer_key: privateConf.apiKey,
+    consumer_secret: privateConf.apiSecret,
+    access_token: privateConf.accessToken,
+    access_token_secret: privateConf.accessTokenSecret,
+    timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+
+twat.post('statuses/update', { status: 'im a vampire'}, function(err, data, response) {
+    console.log('response.statusCode', response.statusCode);
+    console.log(response.statusCode == '200' ? '\n####SUCCESS' : '\n####ERROR')
+    if (err) console.log(err);
+})
